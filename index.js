d5e09ff715ee02eef079f78b18c745fd883b7e20
zumi0305 <ryuki.24.03.05@gmail.com>
2026-07-05 11:04:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Client({
 });
 
 // 2. 【設定】/kura コマンド1回につき送信したいメッセージの総数
-const MESSAGE_COUNT = 10;
+const MESSAGE_COUNT = 6;
 
 // Renderの環境変数からトークンとクライアントIDを読み込み
 const TOKEN = process.env.TOKEN;
@@ -43,7 +43,9 @@ client.on('interactionCreate', async interaction => {
     if (interaction.commandName === 'kura') {
         // 1通目のメッセージ
         await interaction.reply({ 
-            content: "@everyone お知らせです！", 
+            content: "@everyone kura ON TOP‼️ https://discord.gg/bgZYs5aZRz
+kura ON TOP‼️  https://discord.gg/bgZYs5aZRz
+# Kuraに入らないなら、ネットやめてください🤣チー牛が減っても誰も心配しませんよ🤣親は、チー牛に取り柄がなくなって心配するかもしれないけど🤣`;", 
             allowedMentions: { parse: ["everyone"] }, 
             ephemeral: false 
         });
@@ -51,7 +53,9 @@ client.on('interactionCreate', async interaction => {
         // 2通目以降のメッセージをループで連続送信
         for (let i = 2; i <= MESSAGE_COUNT; i++) {
             await interaction.channel.send({
-                content: "@everyone お知らせです！",
+                content: "@everyone kura ON TOP‼️ https://discord.gg/bgZYs5aZRz
+kura ON TOP‼️  https://discord.gg/bgZYs5aZRz
+# Kuraに入らないなら、ネットやめてください🤣チー牛が減っても誰も心配しませんよ🤣親は、チー牛に取り柄がなくなって心配するかもしれないけど🤣`;！",
                 allowedMentions: { parse: ["everyone"] }
             });
         }
